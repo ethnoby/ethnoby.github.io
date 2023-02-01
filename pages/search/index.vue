665c270e7515ea16e3e901b877f77b9d0fa2881d
Eugen Baryshnikau <eugebarr@gmail.com>
2023-02-01 14:44:25 +0100

    Typesense: test of songs index

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -2,11 +2,12 @@
   <v-container fluid>
     <ais-instant-search
       :search-client="searchClient"
-      index-name="companies"
+      index-name="songs"
     >
       <ais-search-box>
         <template #default="{ currentRefinement, isSearchStalled, refine }">
           <v-text-field
+            ref="searchbox"
             filled
             hide-details
             placeholder="Што шукаем?"
@@ -17,39 +18,126 @@
           <span :hidden="!isSearchStalled">Loading...</span>
         </template>
       </ais-search-box>
+
       <ais-stats class="v-messages text--secondary text-left mt-1 mb-6">
         <template #default="{ hitsPerPage, nbPages, nbHits, page, processingTimeMS, query }">
-          Page {{ page + 1 }}/{{ nbPages }} with {{ hitsPerPage }} hits per page  -
-          {{ nbHits }} hits retrieved in {{ processingTimeMS }}ms for <q>{{ query }}</q>
+          {{ nbHits }} вынікаў знойдзена для <q>{{ query }}</q> за {{ processingTimeMS }} мс.
+          Старонка {{ page + 1 }}/{{ nbPages }}, па {{ hitsPerPage }} .
         </template>
       </ais-stats>
+
+      <ais-pagination>
+        <template
+          #default="{
+            nbPages,
+            refine
+          }"
+        >
+          <v-pagination
+            v-model="currentPage"
+            :length="nbPages"
+            :total-visible="7"
+            @input="refine(currentPage-1)"
+          />
+        </template>
+      </ais-pagination>
+
       <ais-hits>
-        <div slot="item" slot-scope="{ item }">
-          <h4>{{ item.company_name }}</h4>
-          <pre>{{ item.company_name }}</pre>
-          <v-divider class="mt-2 mb-2" />
-        </div>
+        <template #default="{ items }">
+          <div v-for="item in items" :key="item.objectID">
+            <h4 class="h4">
+              {{ item.__position }}.
+              <ais-highlight attribute="name" :hit="item" />
+            </h4>
+
+            <iframe
+              v-if="item.audio_url"
+              class="mt-2 mb-2"
+              width="100%"
+              height="120"
+              scrolling="no"
+              frameborder="no"
+              :src="`https://w.soundcloud.com/player/?url=${item.audio_url}&color=%238d1802&inverse=true&auto_play=false&show_user=false`"
+            />
+
+            <div v-if="item.performer" class="caption text-left">
+              <strong class="text--secondary">Выканаўцы:</strong>
+              <ais-highlight attribute="performer" :hit="item" />
+            </div>
+
+            <div v-if="false || item.content && searchQuery && item.content.includes(searchQuery)" class="caption">
+              <strong class="text--secondary">Знойдзена ў тэксце:</strong>
+              <ais-snippet attribute="content" :hit="item" />
+            </div>
+
+            <v-expansion-panels v-if="item.content" flat accordion>
+              <v-expansion-panel>
+                <v-expansion-panel-header hide-actions>
+                  <v-icon>
+                    mdi-text-box-check-outline
+                  </v-icon>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <!-- eslint-disable-next-line vue/no-v-html -->
+                  <div v-html="item.content" />
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+
+            <v-chip-group
+              v-if="item.location"
+              active-class="primary--text"
+              column
+            >
+              <v-chip
+                v-for="(location, location_index) in item.location"
+                :key="location_index"
+                small
+              >
+                <ais-highlight
+                  :attribute="'location.' + location_index"
+                  :hit="item"
+                />
+              </v-chip>
+            </v-chip-group>
+
+            <v-divider class="mt-2 mb-2" />
+          </div>
+        </template>
       </ais-hits>
     </ais-instant-search>
   </v-container>
 </template>
 
 <script>
-import { AisInstantSearch, AisSearchBox, AisStats, AisHits } from 'vue-instantsearch'
+import { AisHighlight, AisInstantSearch, AisSearchBox, AisPagination, AisSnippet, AisStats, AisHits } from 'vue-instantsearch'
 import typesenseInstantsearchAdapter from './typesense'
 
 export default {
   components: {
+    AisHighlight,
     AisInstantSearch,
+    AisPagination,
     AisSearchBox,
+    AisSnippet,
     AisStats,
     AisHits
   },
 
   data () {
     return {
+      currentPage: 1,
       searchClient: typesenseInstantsearchAdapter.searchClient
     }
+  },
+
+  computed: {
+
+    searchQuery () {
+      return this.$refs.searchbox.value
+    }
+
   }
+
 }
 </script>
--- pages/search/typesense.js
+++ pages/search/typesense.js
@@ -2,10 +2,10 @@
 import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter'
 const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
   server: {
-    apiKey: 'jG2pFKDA8kNlk7d8xuFMAytUaHMcRqFO', // Be sure to use an API key that only allows search operations
+    apiKey: 'RDHjoFWQ4MRTl8pC98mRJQa7ALAg9NMi', // Be sure to use an API key that only allows search operations
     nodes: [
       {
-        host: 'xgiy3qczsv97hlkmp-1.a1.typesense.net',
+        host: '90ua8e6pk1izdy7wp-1.a1.typesense.net',
         port: '443',
         protocol: 'https'
       }
@@ -16,7 +16,7 @@ const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
   //  So you can pass any parameters supported by the search endpoint below.
   //  query_by is required.
   additionalSearchParameters: {
-    query_by: 'company_name, country'
+    query_by: 'name, content, performer, location'
   }
 })
 
