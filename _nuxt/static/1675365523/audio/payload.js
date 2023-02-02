a1341ae6b7388909eaba1d560dfb04be44d2406b
skepsys <skepsys@users.noreply.github.com>
2023-02-02 19:23:01 +0000

    deploy: 7d8b72b51901c69e6ad60b35a62eba913c693546

--- _nuxt/static/1675365523/audio/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/audio", {data:[{}],fetch:{},mutations:[]});
--- _nuxt/static/1675365523/video/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fethnoby-nuxt-test\u002F_nuxt\u002Fstatic\u002F1675365523",layout:"default",error:a,state:{user:false},serverRendered:true,routePath:"\u002Fvideo",config:{_app:{basePath:"\u002Fethnoby-nuxt-test\u002F",assetsPath:"\u002Fethnoby-nuxt-test\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"35cd0a48"}}}}(null));
