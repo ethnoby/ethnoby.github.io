cb0f3bcfd20cac1de6e483440e26359461ac55cd
AnastasiyaTolstsik <AnastasiyaTolstsik@users.noreply.github.com>
2023-09-16 14:14:17 +0000

    deploy: 5479976fdbc741346338fe40acaeb0458f66f7e5

---
+++ _nuxt/c453df0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{508:function(n,t){},510:function(n,t,e){"use strict";e.r(t);var o=e(524),c=new o.Client({nodes:[{host:"en4xjabqzl9v0pr1p-1.a1.typesense.net",port:"443",protocol:"https"}],apiKey:"wPjUc5C5JzICtlt24yd50hcurSyNsEnA",connectionTimeoutSeconds:2});t.default=c}}]);
