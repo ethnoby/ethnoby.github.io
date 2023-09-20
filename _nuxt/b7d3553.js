7d7ca6fbc8acdb0ffc25aa6a1dc84e42c1ebf2b0
AnastasiyaTolstsik <AnastasiyaTolstsik@users.noreply.github.com>
2023-09-20 19:43:46 +0000

    deploy: 49af14de5d401088b2fe8386ffa5cb294aed482d

--- _nuxt/b7d3553.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see LICENSES */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[36],{634:function(e,n,r){"use strict";r.r(n);var o=r(520);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.15.0","app-compat")}}]);
