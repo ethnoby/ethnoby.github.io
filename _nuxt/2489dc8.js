2e4e00b4335cda08b0cfe6a5b30b2ea39f77d090
skepsys <skepsys@users.noreply.github.com>
2023-10-27 15:50:17 +0000

    deploy: dcfc0c26eb4f1a121d16f619a7f13de61a960bd6

---
+++ _nuxt/2489dc8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{540:function(n,t){},564:function(n,t,e){"use strict";e.r(t);var o=e(583),c=new o.Client({nodes:[{host:"en4xjabqzl9v0pr1p-1.a1.typesense.net",port:"443",protocol:"https"}],apiKey:"wPjUc5C5JzICtlt24yd50hcurSyNsEnA",connectionTimeoutSeconds:2});t.default=c}}]);
