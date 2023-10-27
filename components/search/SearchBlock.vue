6821ea46877103f854476428559b18d48e54f8e2
Eugen Baryshnikau <eugebarr@gmail.com>
2023-10-27 17:47:34 +0200

    Add leaflet map to search page

--- components/search/SearchBlock.vue
+++ components/search/SearchBlock.vue
@@ -112,81 +112,118 @@
         Старонка {{ page + 1 }}/{{ nbPages }}, па {{ hitsPerPage }} .
       </template>
     </ais-stats>
+    <ais-hits-per-page
+      :items="[
+        { label: '10 hits per page', value: 10, default: true },
+        { label: '25 hits per page', value: 25 },
+        { label: '50 hits per page', value: 50 },
+        { label: '100 hits per page', value: 100 },
+      ]"
+    />
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12" md="6">
+          <ais-hits :escape-h-t-m-l="false">
+            <template #default="{ items }">
+              <div v-for="item in items" :key="item.objectID">
+                <v-expansion-panels>
+                  <v-expansion-panel v-if="item.content" flat accordion>
+                    <v-expansion-panel-header>
+                      <v-img
+                        v-if="item.thumbnail_url"
+                        :src="item.thumbnail_url"
+                        class="mr-6"
+                        max-width="60px"
+                      />
+                      <div class="text-left">
+                        <div class="d-flex text-left" @click.stop>
+                          <nuxt-link :to="`songs/${item.id}/`" class="text--secondary">
+                            <ais-highlight attribute="name" :hit="item" class="text-center text-body-1 text--primary" />
+                          </nuxt-link>
+                        </div>
+                        <div class="text-left mt-1 text-body-2">
+                          <span class="text--secondary">
+                            {{
+                              item.location ? item.location[0] : item.document.location[0]
+                            }}
+                          </span>
+                        </div>
+                        <div v-if="item._highlightResult.content_nohtml.matchedWords.length">
+                          {{ item._highlightResult.content_nohtml.matchedWords }}
+                        </div>
+                        <div v-if="item.content && searchQuery && item.content_nohtml.includes(item._highlightResult.content_nohtml.matchedWords[0])" class="caption mt-3">
+                          <strong class="text--secondary">Знойдзена ў тэксце:</strong>
+                          <ais-snippet attribute="content_nohtml" :hit="item" />
+                        </div>
+                      </div>
+                    </v-expansion-panel-header>
 
-    <ais-hits :escape-h-t-m-l="false">
-      <template #default="{ items }">
-        <div v-for="item in items" :key="item.objectID">
-          <v-expansion-panels>
-            <v-expansion-panel v-if="item.content" flat accordion>
-              <v-expansion-panel-header>
-                <v-img
-                  v-if="item.thumbnail_url"
-                  :src="item.thumbnail_url"
-                  class="mr-6"
-                  max-width="60px"
+                    <v-expansion-panel-content>
+                      <!--                {{ item._highlightResult }}-->
+                      <!-- eslint-disable-next-line vue/no-v-html -->
+                      <div class="text--primary" v-html="item._highlightResult.content.value" />
+                      <div v-if="item.performer" class="caption text-left">
+                        <strong class="text--secondary">Выканаўцы:</strong>
+                        <ais-highlight attribute="performer" :hit="item" />
+                      </div>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                </v-expansion-panels>
+              </div>
+            </template>
+          </ais-hits>
+
+          <ais-pagination class="mt-6">
+            <template
+              #default="{
+                nbPages,
+                nbHits,
+                refine
+              }"
+            >
+              <v-pagination
+                v-if="nbHits"
+                v-model="currentPage"
+                :length="nbPages"
+                :total-visible="7"
+                @input="refine(currentPage-1)"
+              />
+              <v-banner
+                v-else
+                icon="mdi-magnify-expand"
+                icon-color="error"
+              >
+                Па запыце <strong><q>{{ searchQuery }}</q></strong> у нас ніц няма, спрабуйце іначай
+              </v-banner>
+            </template>
+          </ais-pagination>
+        </v-col>
+
+        <v-col v-if="nbHits" cols="12" md="6">
+          <v-responsive aspect-ratio="1">
+            <client-only>
+              <l-map :zoom="6" :center="[53.893009, 27.567444]">
+                <l-tile-layer
+                  url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                  attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
                 />
-                <div class="text-left">
-                  <div class="d-flex text-left" @click.stop>
-                    <nuxt-link :to="`songs/${item.id}/`" class="text--secondary">
-                      <ais-highlight attribute="name" :hit="item" class="text-center text-body-1 text--primary" />
-                    </nuxt-link>
-                  </div>
-                  <div class="text-left mt-1 text-body-2">
-                    <span class="text--secondary">
-                      {{
-                        item.location ? item.location[0] : item.document.location[0]
-                      }}
-                    </span>
-                  </div>
-                  <div v-if="item._highlightResult.content_nohtml.matchedWords.length">
-                    {{ item._highlightResult.content_nohtml.matchedWords }}
-                  </div>
-                  <div v-if="false || item.content && searchQuery && item.content_nohtml.includes(item._highlightResult.content_nohtml.matchedWords[0])" class="caption mt-3">
-                    <strong class="text--secondary">Знойдзена ў тэксце:</strong>
-                    <ais-snippet attribute="content_nohtml" :hit="item" />
-                  </div>
-                </div>
-              </v-expansion-panel-header>
-
-              <v-expansion-panel-content>
-                <!--                {{ item._highlightResult }}-->
-                <!-- eslint-disable-next-line vue/no-v-html -->
-                <div class="text--primary" v-html="item._highlightResult.content.value" />
-                <div v-if="item.performer" class="caption text-left">
-                  <strong class="text--secondary">Выканаўцы:</strong>
-                  <ais-highlight attribute="performer" :hit="item" />
-                </div>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </div>
-      </template>
-    </ais-hits>
-
-    <ais-pagination class="mt-6">
-      <template
-        #default="{
-          nbPages,
-          nbHits,
-          refine
-        }"
-      >
-        <v-pagination
-          v-if="nbHits"
-          v-model="currentPage"
-          :length="nbPages"
-          :total-visible="7"
-          @input="refine(currentPage-1)"
-        />
-        <v-banner
-          v-else
-          icon="mdi-magnify-expand"
-          icon-color="error"
-        >
-          Па запыце <strong><q>{{ searchQuery }}</q></strong> у нас ніц няма, спрабуйце іначай
-        </v-banner>
-      </template>
-    </ais-pagination>
+                <ais-hits :escape-h-t-m-l="false">
+                  <template #default="{ items }">
+                    <template v-for="item in items">
+                      <l-marker v-if="item.geo" :key="item.objectID" :lat-lng="geoToArray(item.geo)">
+                        <l-popup>
+                          {{ item.location ? item.location[0] : item.document.location[0] }}
+                        </l-popup>
+                      </l-marker>
+                    </template>
+                  </template>
+                </ais-hits>
+              </l-map>
+            </client-only>
+          </v-responsive>
+        </v-col>
+      </v-row>
+    </v-container>
   </ais-instant-search>
 </template>
 
@@ -195,6 +232,7 @@ import {
   AisHierarchicalMenu,
   AisHighlight,
   AisHits,
+  AisHitsPerPage,
   AisInstantSearch,
   AisPagination, AisRefinementList,
   AisSearchBox,
@@ -205,15 +243,16 @@ import typesenseInstantsearchAdapter from './typesense'
 
 export default {
   components: {
+    AisHierarchicalMenu,
     AisHighlight,
+    AisHits,
+    AisHitsPerPage,
     AisInstantSearch,
     AisPagination,
+    AisRefinementList,
     AisSearchBox,
     AisSnippet,
-    AisStats,
-    AisHits,
-    AisHierarchicalMenu,
-    AisRefinementList
+    AisStats
   },
 
   props: {
