0dec1e84d125b761a049183a6b4e33ec2bfe8f2a
ANASTASIYA TOLSTSTIK <anastasiya.tolstik@workhuman.com>
2023-11-13 23:18:37 +0200

    Searchable link Test - return master version

--- components/search/SearchBlock.vue
+++ components/search/SearchBlock.vue
@@ -2,10 +2,7 @@
   <ais-instant-search
     :search-client="searchClient"
     :index-name="collection"
-    :initial-ui-state="initialUiState"
   >
-    <ais-configure :hits-per-page="24" :filters="algoliaFilters" />
-
     <v-container fluid>
       <v-row>
         <v-col cols="12" md="4">
@@ -48,7 +45,6 @@
               }"
             >
               <v-select
-                v-model="selected"
                 :items="items"
                 label="Жанры"
                 multiple
@@ -225,18 +221,8 @@ export default {
 
   data () {
     return {
-      currentPage: 5,
-      searchClient: typesenseInstantsearchAdapter.searchClient,
-      initialUiState: {
-        songs: {
-          query: this.$route.query.q,
-          // query_by: 'name, content, performer, location, content_nohtml',
-          refinementList: {
-            tags: [this.$route.query.tag]
-          }
-        }
-      },
-      selected: this.$route.query.tag
+      currentPage: 1,
+      searchClient: typesenseInstantsearchAdapter.searchClient
     }
   },
 
@@ -250,10 +236,6 @@ export default {
 
     tags () {
       return this.items
-    },
-
-    currentRefinement () {
-      return 'kalyna'
     }
 
   },
