2562fb5e685baa1bb8ba5c54044677be40e90eb2
ANASTASIYA TOLSTSTIK <anastasiya.tolstik@workhuman.com>
2024-04-16 22:33:45 +0300

    Add embedded youtube

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,6 +50,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '@/plugins/youtube.js', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
