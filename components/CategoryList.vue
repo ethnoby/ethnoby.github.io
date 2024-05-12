2c89e155f19531783a6d869ef7fcd346eb7ad38e
ANASTASIYA TOLSTSTIK <anastasiya.tolstik@workhuman.com>
2024-05-12 22:10:15 +0300

    Add main layout - locations

---
+++ components/CategoryList.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <div><h1>{{ title }}</h1></div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="card in cards" :key="card.title" :cols="card.flex" :lg="card.lg">
+        <v-card>
+          <nuxt-link
+            class="text-decoration-none"
+            :to="card.toLink"
+          >
+            <v-img
+              :src="card.src"
+              class="align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="100%"
+              cover
+            >
+              <v-card-title
+                class="white--text"
+              >
+                {{ card.title }}
+              </v-card-title>
+            </v-img>
+          </nuxt-link>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'CategoryList',
+  props: {
+    cards: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+
+  }
+}
+</script>
